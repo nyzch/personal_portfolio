50027ad350f81e483fd5e1099e320f7a2981901d
Nancy Zhong <nancyz@andrew.cmu.edu>
2023-12-20 18:20:37 -0500

    Finished Projects and Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,27 @@
+import { Container, Row, Col } from "react-bootstrap";
+import logo from "../assets/img/logo.png";
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
+
+export const Footer = () => {
+    return (
+        <footer className="footer">
+            <Container>
+                <Row className="align-item-center">
+                    <Col sm={6}>
+                        <img src={logo} alt="Logo"></img>
+                    </Col>
+                    <Col sm={6} className="text-center text-sm-end">
+                        <div className="social-icon">
+                            <a href="https://www.linkedin.com/in/nancy-zhong-4184bb246/"><img src={navIcon1} /></a>
+                            <a href="https://www.linkedin.com/in/nancy-zhong-4184bb246/"><img src={navIcon2} /></a>
+                            <a href="https://www.linkedin.com/in/nancy-zhong-4184bb246/"><img src={navIcon3} /></a>
+                        </div>
+                        <p>Copyright 2023. All rights reserved.</p>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    )
+}
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,9 @@
 import { Container, Col, Row, Nav, Tab } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
 
 export const Projects = () => {
     const projects = [
@@ -14,16 +17,36 @@ export const Projects = () => {
             description: "Second project",
             imgUrl: projImg2,
         },
+        {
+            title: "Proj 3",
+            description: "Third project",
+            imgUrl: projImg3,
+        },
+        {
+            title: "Proj 4",
+            description: "Fourth project",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Proj 5",
+            description: "Fifth project",
+            imgUrl: projImg2,
+        },
+        {
+            title: "Proj 6",
+            description: "Sixth project",
+            imgUrl: projImg3,
+        },
     ];
     return (
         <section className="project" id="project">
             <Container>
                 <Row>
-                    <Col>
+                    <Col size={12}>
                         <h2>Projects</h2>
                         <p>Here are some projects I've worked on</p>
                         <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                            <Nav variant="pills" className="flex-column">
+                            <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                                 <Nav.Item>
                                     <Nav.Link eventKey="first">Tab 1</Nav.Link>
                                 </Nav.Item>
@@ -40,18 +63,23 @@ export const Projects = () => {
                                         {
                                             projects.map((project, index) => {
                                                 return (
-                                                    <p>{project.title}</p>
+                                                    <ProjectCard
+                                                        key={index}
+                                                        {...project}
+                                                    />
                                                 )
                                             })
                                         }
                                     </Row>
                                 </Tab.Pane>
                                 <Tab.Pane eventKey="second">Second tab content</Tab.Pane>
+                                <Tab.Pane eventKey="third">Third tab content</Tab.Pane>
                             </Tab.Content>
                         </Tab.Container>
                     </Col>
                 </Row>
             </Container>
+            <img className="background-image-right" src={colorSharp2}></img>
         </section >
     )
 }
